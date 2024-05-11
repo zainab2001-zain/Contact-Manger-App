bc79031b992600cd769b91a96b7dd268aa64e9cd
zainab2001-zain <63280336+zainab2001-zain@users.noreply.github.com>
2024-05-11 15:27:22 +0500

    add project to git

---
+++ sqlitecrud/app/src/main/java/com/example/sqlitecrud/model.java
@@ -0,0 +1,36 @@
+package com.example.sqlitecrud;
+
+public class model
+{
+  String name,contact,email;
+
+    public model(String name, String contact, String email) {
+        this.name = name;
+        this.contact = contact;
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getContact() {
+        return contact;
+    }
+
+    public void setContact(String contact) {
+        this.contact = contact;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
